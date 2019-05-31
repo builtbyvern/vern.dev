478b71a636d8e9c119c2c652a09d99e63bf2438c
John Briggs <vernworldwide@gmail.com>
2019-05-31 11:51:13 -0700

    add react-icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.scss'
+import { FaGithub, FaBitbucket, FaPhone, FaEnvelope } from "react-icons/fa"
 
 // TODO animate head to follow mouse cursor
 // TODO animate in circled around head
@@ -7,10 +8,15 @@ import './Header.scss'
 
 function Header() {
  return (
-   <header>
-    <div className="logo">Logo</div>
-    <div className="links">Links</div>
-   </header>
+    <header>
+      <div className="logo">Logo</div>
+      <div className="links">
+        <FaGithub />
+        <FaBitbucket />
+        <FaPhone />
+        <FaEnvelope />
+      </div>
+    </header>
   )
 }
 
